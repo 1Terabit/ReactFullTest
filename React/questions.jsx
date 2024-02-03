6a59cb279a4f44e63f100ef817dc47765d9a6e69
Edwar Susarra <edwar.susarra@externos-cl.cencosud.com>
2024-02-03 00:11:08 -0400

    ADD: React and Node Answers

--- React/questions.jsx
+++ React/questions.jsx
@@ -1,6 +1,6 @@
-//FIXME: 1. Crea un componente de clase llamado `Reloj` que muestre la hora actual y se actualice cada segundo.
+1. Crea un componente de clase llamado `Reloj` que muestre la hora actual y se actualice cada segundo.
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"
 
 function Clock() {
   const [currentTime, setCurrentTime] = useState(
@@ -24,11 +24,11 @@ function Clock() {
 
 export default Clock;
 
-//FIXME: 2. Estado y Ciclo de Vida:
+2. ¿Cuándo usarías el método `shouldComponentUpdate` en un componente de clase y por qué?
 
     console.info("shouldComponentUpdate: Se usa para evitar que un componente se actualice innecesariamente. Se debe usar cuando el componente solo necesita actualizarse cuando sus props o estado cambian de una manera que afecta su renderizado")
 
-//FIXME: 3. En un componente, crea una función `manejarSubmit` que se ejecute al enviar un formulario y evite que la página se recargue. 
+3. En un componente, crea una función `manejarSubmit` que se ejecute al enviar un formulario y evite que la página se recargue. 
 
 
 function App() {
@@ -71,14 +71,14 @@ function App() {
   );
 }
 
-//FIXME: 4. Explica la diferencia entre `map` y `forEach` al trabajar con listas en React. 
+4. Explica la diferencia entre `map` y `forEach` al trabajar con listas en React. 
 
 console.info("map: Crea una nueva lista a partir de la original, transformando cada elemento")
 console.info("forEach: Recorre la lista original y ejecuta una función para cada elemento")
 
-//FIXME: 5. Crea un componente de entrada de texto controlado que acepte solo números enteros positivos. 
+5. Crea un componente de entrada de texto controlado que acepte solo números enteros positivos. 
 
-import React from 'react';
+import React from 'react'
 
 function PositiveIntegerInput({ value, onChange }) {
   const [inputValue, setInputValue] = useState(value);
@@ -98,24 +98,24 @@ function PositiveIntegerInput({ value, onChange }) {
 
 export default PositiveIntegerInput;
 
-//FIXME: 6. Hooks:
+6. ¿Qué hace la función `useEffect` en un componente funcional de React? Proporciona un ejemplo práctico
 
 console.info("useEffect: Permite ejecutar un efecto después de que un componente se ha montado o actualizado")
 
-//FIXME: 7. Enrutamiento:
+7. Explica la diferencia entre `BrowserRouter` y `HashRouter` en `react-router-dom`.
 
 console.info("BrowserRouter: Usa el historial del navegador para navegar entre diferentes páginas")
 console.info("HashRouter: Usa la parte hash de la URL para navegar entre diferentes páginas")
 
-//FIXME: 8. Context API:
+8. ¿Cuándo y por qué usarías el Context API en React? Proporciona un ejemplo de su implementación. 
 
 console.info("Se usa para compartir datos entre componentes que no son padres e hijos directos")
 
-//FIXME: 9. HOC (Higher Order Component):
+9. ¿Qué es un HOC en React y cómo se utiliza? Proporciona un ejemplo.
 
 console.info("Es un componente que se utiliza para envolver otro componente y agregarle funcionalidad adicional")
 
-//FIXME: 10. Testing en React:
+10. Menciona al menos dos bibliotecas populares para realizar pruebas unitarias en aplicaciones React. 
 
 console.info("Jest: Biblioteca popular para realizar pruebas unitarias en aplicaciones React")
 console.info("react Testing Library: Biblioteca que facilita la escritura de pruebas para componentes React")
